71a7459f4b188ff058d1211a8a58bc50acfe34f1
Ben Martin <bmartin2009@gmail.com>
2022-02-25 23:30:53 -0600

    Minor fixes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Footer() {
   return (
     <div id="footer">
-      <p><a href="https://github.com/the-wake/" target="_blank">LinkedIn</a></p>
-      <p><a href="https://www.linkedin.com/in/ben-martin-26266443/" target="_blank">GitHub</a></p>
+      <p><a href="https://www.linkedin.com/in/ben-martin-26266443/" target="_blank">LinkedIn</a></p>
+      <p><a href="https://github.com/the-wake/" target="_blank">GitHub</a></p>
       <p><a href="https://boardgamegeek.com/user/bmartin2009" target="_blank">BoardGameGeek</a></p>
     </div>
   )
